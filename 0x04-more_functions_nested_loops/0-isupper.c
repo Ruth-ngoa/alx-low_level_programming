4bc5db35dc2564fca8a681328c81a5ee7ad7c634
ngoa <buundaruth@gmail.com>
2022-12-16 00:49:53 -0800

    more functions nested loops task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * _isupper: checks for uppercase characters
+ * @c: the characters to be checked
+ * Return : 1 if the character is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= ' 0 ' && c <= ' 9 ')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}	
