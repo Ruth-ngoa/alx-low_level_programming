6f7aee9c3175a16a0ae360a036aaec2061dfe684
ngoa <buundaruth@gmail.com>
2023-01-17 04:27:05 -0800

    my commit task 0

---
+++ 0x0E-structures_typedef/_putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * _putchar: writes the character c to stdout
+ * @c: the character to print
+ * return: on succes!
+ * on error: 1 is retured, and erno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1;&c,1));
+}
